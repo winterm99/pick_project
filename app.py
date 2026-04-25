ee288130311a1d1c8bf0c581bc952324b172025f
rosealfred123 <rose.alfred2005@gmail.com>
2026-04-25 11:12:32 +0200

    kommentare in py für verständnis

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+# Imports
+
 from flask import Flask, render_template, request, redirect
 import random
 import string
+# notwendig um in render variablen zu lesen !!
+import os
 
 app = Flask(__name__)
 
 polls = {}
+
+
+# Routen
 
 @app.route("/")
 def index():
@@ -26,51 +33,70 @@ def help():
     return render_template("help.html")
 
 
+# Code erstellen
+
 def generate_code(length=6):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
 
 
+# Umfrage erstellen
+
 @app.route("/create", methods=["POST"])
 def create():
-    question = request.form.get("frage")
+
+    # aus html daten holen
+    question = request.form.get("question")
     answers = request.form.getlist("answer")
 
+    # keine leeren Umfragen
     if not question or not answers:
         return "Invalid input"
 
     code = generate_code()
 
+    # Daten speichern 
     polls[code] = {
         "question": question,
         "answers": answers,
         "votes": [0] * len(answers)
     }
 
+    # Fügt code in Url ein  und leitet weiter
     return redirect(f"/poll/{code}")
 
 
+# Umfrage anzeigen (<code> ist in der url)
+
 @app.route("/poll/<code>")
 def poll(code):
+                       
     poll = polls.get(code)
 
     if not poll:
         return "Poll not found"
 
+    # html anzeigen
     return render_template("poll.html", poll=poll, code=code)
 
 
 @app.route("/join", methods=["POST"])
 def join():
+
+    # code holen in großbuchstaben
     code = request.form.get("code", "").upper()
 
+    # prüfen
     if not code:
         return "Please enter a code"
 
     if code not in polls:
         return "Invalid code"
 
+    # weiterleiten zu choose seite
     return redirect(f"/choose/{code}")
 
+
+# Seite zum abstimmen
 
 @app.route("/choose/<code>")
 def choose(code):
@@ -81,6 +107,9 @@ def choose(code):
 
     return render_template("choose.html", poll=poll, code=code)
 
+
+# Stimme abgeben 
+
 @app.route("/vote/<code>", methods=["POST"])
 def vote(code):
     poll = polls.get(code)
@@ -88,18 +117,23 @@ def vote(code):
     if not poll:
         return "Poll not found"
 
+    # Auswahl holen von html
     choice = request.form.get("vote")
 
+    # Prüfen
     if choice is None:
         return "No option selected"
 
+    # Stimme zählen
     choice = int(choice)
     poll["votes"][choice] += 1
 
+    # zurück zur umfrage übersicht
     return redirect(f"/poll/{code}")
 
 
-import os
+# Server nur starten, wenn app.py direkt ausgeführt wird
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
